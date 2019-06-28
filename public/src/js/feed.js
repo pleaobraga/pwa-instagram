0cede2533cfc99d91a6dfc1b9fed8ae428856cff
Pedro Leâo <pleao.braga@gmail.com>
2019-06-27 21:54:39 -0300

    Added IndexedDB in the project

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -1,30 +1,108 @@
-var shareImageButton = document.querySelector('#share-image-button');
-var createPostArea = document.querySelector('#create-post');
-var closeCreatePostModalButton = document.querySelector('#close-create-post-modal-btn');
+var shareImageButton = document.querySelector("#share-image-button");
+var createPostArea = document.querySelector("#create-post");
+var closeCreatePostModalButton = document.querySelector(
+  "#close-create-post-modal-btn"
+);
+var sharedMomentsArea = document.querySelector("#shared-moments");
 
 function openCreatePostModal() {
-  createPostArea.style.display = 'block';
-  if(deferedPrompt) {
-    deferedPrompt.prompt()
+  createPostArea.style.display = "block";
+  if (deferredPrompt) {
+    deferredPrompt.prompt();
 
-    deferedPrompt.userChoice.then(choiceResult => {
-      console.log(choiceResult.outcome)
+    deferredPrompt.userChoice.then(function(choiceResult) {
+      console.log(choiceResult.outcome);
 
-      if(choiceResult.outcome === 'dismissed') {
-        colsole.log('user cancelled instalation')
+      if (choiceResult.outcome === "dismissed") {
+        console.log("User cancelled installation");
       } else {
-        console.log('user added to home screen')
+        console.log("User added to home screen");
       }
-    })
+    });
 
-    deferedPrompt= null
+    deferredPrompt = null;
   }
 }
 
 function closeCreatePostModal() {
-  createPostArea.style.display = 'none';
+  createPostArea.style.display = "none";
 }
 
-shareImageButton.addEventListener('click', openCreatePostModal);
+shareImageButton.addEventListener("click", openCreatePostModal);
 
-closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
+closeCreatePostModalButton.addEventListener("click", closeCreatePostModal);
+
+// const onSaveButtonClicked = (event) => {
+//   console.log('Clicked here')
+//   if('caches' in window){
+//     caches.open('user-requested')
+//       .then(cache => {
+//         cache.addAll([
+//           'https://httpbin.org/get',
+//           '/src/images/sf-boat.jpg'
+//         ])
+//       })
+//   }
+// }
+
+const clearCards = () => {
+  while (sharedMomentsArea.hasChildNodes()) {
+    sharedMomentsArea.removeChild(sharedMomentsArea.lastChild);
+  }
+};
+
+function createCard(data) {
+  var cardWrapper = document.createElement("div");
+  cardWrapper.className = "shared-moment-card mdl-card mdl-shadow--2dp";
+  var cardTitle = document.createElement("div");
+  cardTitle.className = "mdl-card__title";
+  cardTitle.style.backgroundImage = `url("${data.image}")`;
+  cardTitle.style.backgroundSize = "cover";
+  cardTitle.style.height = "180px";
+  cardWrapper.appendChild(cardTitle);
+  var cardTitleTextElement = document.createElement("h2");
+  cardTitleTextElement.style.color = "white";
+  cardTitleTextElement.className = "mdl-card__title-text";
+  cardTitleTextElement.textContent = data.title;
+  cardTitle.appendChild(cardTitleTextElement);
+  var cardSupportingText = document.createElement("div");
+  cardSupportingText.className = "mdl-card__supporting-text";
+  cardSupportingText.textContent = data.location;
+  cardSupportingText.style.textAlign = "center";
+  // var cardSaveButton = document.createElement('button')
+  // cardSaveButton.textContent = 'Save'
+  // cardSaveButton.addEventListener('click', onSaveButtonClicked)
+  // cardSupportingText.appendChild(cardSaveButton)
+  cardWrapper.appendChild(cardSupportingText);
+  componentHandler.upgradeElement(cardWrapper);
+  sharedMomentsArea.appendChild(cardWrapper);
+}
+
+const updateUI = datas => {
+  datas.forEach(data => {
+    createCard(data);
+  });
+};
+
+var networkDataRecieve = false;
+
+fetch("https://pwa-gram-plbraga.firebaseio.com/posts.json")
+  .then(function(res) {
+    return res.json();
+  })
+  .then(function(data) {
+    networkDataRecieve = true;
+    var dataArray = [];
+    for (var key in data) {
+      dataArray.push(data[key]);
+    }
+    clearCards();
+    updateUI(dataArray);
+  });
+
+if ("indexedDB" in window) {
+  readAllData("posts").then(data => {
+    console.log("From cache", data);
+    updateUI(data);
+  });
+}
